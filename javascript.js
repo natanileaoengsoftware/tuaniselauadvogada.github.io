24c9286b21a16fcc0216fd7970cebacf4eb70323
Natani Germann Selau Leão <natanigselau@gmail.com>
2023-11-30 07:30:44 -0300

    carrosel de fotos

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//funcao whatsapp
 function abrirWhatsapp() {
   var nome = document.getElementById("nome").value;
   var telefone = document.getElementById("telefone").value;
@@ -12,3 +13,38 @@ function abrirWhatsapp() {
     + "*Mensagem*: " + msg;
   window.open(url, '_blank').focus();
 }
+
+//funcao para o carrossel de imagens no inicio do site
+const slides = document.querySelector('.slides');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let slidePosition = 0;
+const slidesCount = slides.childElementCount;
+
+function updateSlidePosition() {
+    for (let slide of slides.children) {
+        slide.style.transform = `translateX(-${slidePosition}px)`;
+    }
+}
+
+function moveToNextSlide() {
+    if (slidePosition === (slidesCount - 1) * slides.children[0].clientWidth) {
+        slidePosition = 0;
+    } else {
+        slidePosition += slides.children[0].clientWidth;
+    }
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = (slidesCount - 1) * slides.children[0].clientWidth;
+    } else {
+        slidePosition -= slides.children[0].clientWidth;
+    }
+    updateSlidePosition();
+}
+
+nextButton.addEventListener('click', moveToNextSlide);
+prevButton.addEventListener('click', moveToPrevSlide);
